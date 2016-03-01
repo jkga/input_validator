b3c5bbcdafd3dca6489067843539da337ad94bf8
kenneth <johnkennethgibasabella@gmail.com>
2016-03-01 13:43:56 +0800

    fixed repeated read results and added email() and contained_in() function

--- validator.php
+++ validator.php
@@ -9,11 +9,9 @@ namespace validator;
 
 class input_validator{
 
-	#return 1 if has error:0
-	static $referenceError=0;
+	static $errors=[];
 	static $message=[];
-	static $saved_data=[];
-
+	static $result=0;
 	#constructor
 	function __construct(){ }
 	function __toString(){
@@ -23,10 +21,13 @@ class input_validator{
 
 		return $message;
 	}
+
 	
 	#read custom message
 	public function read($i=''){
-
+		#read returns singleton if called independently
+		#it will only return value from chained method if called right after writing ex.$sample->write('method#1','fs from chain')->write('method#2','chained method')->read()
+		
 		if($i==''){
 			
 			#return chained function
@@ -34,11 +35,14 @@ class input_validator{
 
 			#loop in each writen message
 			$x=0;
+			$message_count=count($this->message);
 			foreach ($this->message as $key => $value) {
+
 				$messageInChainedFunction[]=$this->message[$key];
 				#add an integer index alongside defined key
-				$this->message[$x]=$this->message[$key];
-				$x++;
+				#check first if index is string to avoid overflow
+				if(gettype($key)==='string') $this->message[$x]=$this->message[$key]; $x++;
+				
 			}
 
 			$messageInChainedFunction=$this->message;
@@ -47,16 +51,19 @@ class input_validator{
 		}else{
 
 			#search in message stack
+			if(isset($this->message))
 			if(array_key_exists($i, $this->message)){
 				return $this->message[$i];
 			} 	
 		} 	
 		
-	}
+	} 
+
+
+
 
 	#write custom message
 	public function write($err,$mess,$clone=false){
-
 		#read the message in current chain method
 		if(!$clone){
 			$this->message[$err]=$mess;
@@ -78,6 +85,61 @@ class input_validator{
 		$this->message=[];
 		return $this; 
 	}
+
+
+	#check if valid email
+	public function email($value=array()){
+		#optional pattern
+		#$pattern='/^([0-9a-zA-Z\_]{3,})+@([a-z]{4,})+(.{1}[a-z]{2,3})$/i'; //old pattern
+		/*(lettersornumbers may contain underscore{min of 3letters})+@(letters{min 4})
+		+(.{max 1 dot})(letters(min 0f 2,max of 3))*/
+			
+			$this->name='email';	
+			$this->data=$value;
+			$this->result=1;
+		
+		foreach ($value as $key => $val) {
+			
+			if(filter_var($val,FILTER_VALIDATE_EMAIL)){
+				#if(isset(self::$errors['email'])) unset(self::$errors['email']);
+			
+			}else{#self::$referenceError=1;
+				$this->error[$key]=$val.' is not a valid email address';
+				self::$errors['email'][$key]=$this->error[$key];
+				$this->result=0;
+			}
+
+
+		}
+
+		return $this;
+
+
+	}
+
+	#check if exist in data
+	public function contained_in($set=array()){
+
+		$this->name='contain';
+		$this->result=0;
+		foreach ($this->data as $key => $val) {
+			#$this->result=0;
+			self::$errors['contain'][$key]=$val.' does not contained in set';
+			if(in_array($this->data[$key],$set)){
+				$this->result=1;
+				unset(self::$errors['contain']);
+				break;
+			}
+
+			
+		}
+		 
+		return $this;
+
+	}
+
+
+
 
 
 
